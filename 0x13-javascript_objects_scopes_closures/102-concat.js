7c6f77b3d63e65f7f6021c522b4e1b0ff57d4378
Fredrickmureti <fredrickmureti612@gmail.com>
2024-06-28 03:18:25 -0700

    second commit

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,22 +1,34 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-const fileA = process.argv[2];
-const fileB = process.argv[3];
-const fileC = process.argv[4];
-
-if (
-  fs.existsSync(fileA) &&
-fs.statSync(fileA).isFile &&
-fs.existsSync(fileB) &&
-fs.statSync(fileB).isFile &&
-fileC !== undefined
-) {
-  const fileAContent = fs.readFileSync(fileA);
-  const fileBContent = fs.readFileSync(fileB);
-  const stream = fs.createWriteStream(fileC);
-
-  stream.write(fileAContent);
-  stream.write(fileBContent);
-  stream.end();
+if (process.argv.length !== 5) {
+  console.error('Usage: node 102-concat.js <fileA> <fileB> <fileC>');
+  process.exit(1);
 }
+
+const [,, fileA, fileB, fileC] = process.argv;
+
+fs.readFile(fileA, 'utf-8', (err, dataA) => {
+  if (err) {
+    console.error(`Error reading file ${fileA}:`, err.message);
+    process.exit(1);
+  }
+
+  fs.readFile(fileB, 'utf-8', (err, dataB) => {
+    if (err) {
+      console.error(`Error reading file ${fileB}:`, err.message);
+      process.exit(1);
+    }
+
+    const result = dataA + dataB;
+
+    fs.writeFile(fileC, result, 'utf-8', (err) => {
+      if (err) {
+        console.error(`Error writing to file ${fileC}:`, err.message);
+        process.exit(1);
+      }
+
+      console.log(`Files ${fileA} and ${fileB} were concatenated into ${fileC}`);
+    });
+  });
+});
